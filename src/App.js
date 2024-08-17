1e0e73f2df2097ab12c56d32ba9c2b52e5a73d03
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-08-17 14:45:30 +0500

    add links a\tags in files that they are not refersh the whole page

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import {
 import './App.css';
 import HomePage from './Pages/HomePage';
 import MainLayout from './Layout/MainLayout';
-
+import JobPage from './Pages/JobPage';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobPage />} />
     </Route>
   )
 );
--- src/Compenents/Hero.jsx
+++ src/Compenents/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Hero({ title, subtitle }) {
   return (
-    <section 
-      className="bg-indigo-700 py-20 mb-4 bg-cover bg-center h-screen" 
+    <section
+      className="bg-indigo-700 py-20 mb-4 bg-cover bg-center h-screen"
       style={{ backgroundImage: `url('https://png.pngtree.com/thumb_back/fh260/background/20210912/pngtree-people-looking-for-jobs-with-words-job-seeker-image_877069.jpg')` }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center h-full">
