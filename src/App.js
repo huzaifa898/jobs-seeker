f74a8dc29755321a0b67c21b1f83f5abb5a85296
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-08-19 13:03:14 +0500

    add job page and use states for maintaing it

--- src/App.js
+++ src/App.js
@@ -11,19 +11,25 @@ import JobsPage from './Pages/JobsPage';
 import NotFoundPage from './Pages/NotFoundPage';
 import JobPage ,{jobLoader} from './Pages/JobPage';
 import AddJobPage from './Pages/AddJobPage';
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-);
+
+ 
 
 function App() {
+  const addJob = (newJob) => {
+
+  }
+ 
+ const router = createBrowserRouter(
+   createRoutesFromElements(
+     <Route path="/" element={<MainLayout />}>
+       <Route index element={<HomePage />} />
+       <Route path='/jobs' element={<JobsPage />} />
+       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+       <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+       <Route path='*' element={<NotFoundPage />} />
+     </Route>
+   )
+ );
   return (
     <RouterProvider router={router} />
   );
